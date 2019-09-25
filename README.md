164a61e719bd435086e91af1cb91eeb4432685bd
Moses Gitau <Moses.Gitau@inm.corp>
2019-09-25 15:46:15 +0300

    Set up the configuration

---
+++ README.md
@@ -0,0 +1,19 @@
+# Spring CRUD Application
+
+This application contains the student form  that includes the CRUD features such as add, view, delete, and update student.
+
+It is based on Spring boot to handle the api calls.
+
+## Database
+ 
+ - MySQL
+
+## Server
+ 
+ - Apache Tomcat/ JBoss/ Glassfish/ Weblogic/ Websphere
+
+## Technologies
+
+1. SpringBoot
+2. Hibernate
+3. MySQL
---
+++ src/main/java/config/Config.java
@@ -0,0 +1,69 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.spel.support.DataBindingPropertyAccessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
+@ComponentScans(value = {
+        @ComponentScan("boot.entry"),
+        @ComponentScan("Model"),
+        @ComponentScan("Controller"),
+        @ComponentScan("DAO"),
+        @ComponentScan("Miscallaneous"),
+        @ComponentScan("Service")
+})
+public class Config {
+
+    @Value("${db.driver}")
+    private String DB_DRIVER;
+
+    @Value("${db.password}")
+    private String DB_PASSWORD;
+
+    @Value("${db.url}")
+    private String DB_URL;
+
+    @Value("${db.username}")
+    private String DB_USERNAME;
+
+    @Value("${hibernate.dialect}")
+    private String HIBERNATE_DIALECT;
+
+    @Value("${hibernate.show_sql}")
+    private String HIBERNATE_SHOW_SQL;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String HIBERNATE_HBM2DDL_AUTO;
+
+    @Value("${entitymanager.packagesToScan}")
+    private String ENTITYMANAGER_PACKAGES_TO_SCAN;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+        return dataSource;
+    }
+}
